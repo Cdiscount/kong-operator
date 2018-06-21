92a3f9e8a4871b64f008ff0d869eb534ebd99e59
Etienne Coutaud <e.coutaud@gmail.com>
2018-06-21 10:18:34 +0200

    Add api/type test

--- pkg/apis/apim/v1alpha1/types_test.go
+++ pkg/apis/apim/v1alpha1/types_test.go
@@ -13,3 +13,62 @@
 // limitations under the License.
 
 package v1alpha1
+
+import (
+	"math/rand"
+	"testing"
+
+	"github.com/google/gofuzz"
+
+	"k8s.io/apimachinery/pkg/api/testing/fuzzer"
+	roundtrip "k8s.io/apimachinery/pkg/api/testing/roundtrip"
+	metafuzzer "k8s.io/apimachinery/pkg/apis/meta/fuzzer"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/runtime"
+	"k8s.io/apimachinery/pkg/runtime/serializer"
+	runtimeserializer "k8s.io/apimachinery/pkg/runtime/serializer"
+)
+
+var _ runtime.Object = &KongService{}
+var _ metav1.ObjectMetaAccessor = &KongService{}
+
+var _ runtime.Object = &KongRoute{}
+var _ metav1.ObjectMetaAccessor = &KongRoute{}
+
+var _ metav1.ListMetaAccessor = &KongRouteList{}
+var _ metav1.ListMetaAccessor = &KongServiceList{}
+
+func apimFuzzerFuncs(codecs runtimeserializer.CodecFactory) []interface{} {
+	return []interface{}{
+		func(obj *KongRouteList, c fuzz.Continue) {
+			c.FuzzNoCustom(obj)
+			obj.Items = make([]KongRoute, c.Intn(10))
+			for i := range obj.Items {
+				c.Fuzz(&obj.Items[i])
+			}
+		},
+		func(obj *KongServiceList, c fuzz.Continue) {
+			c.FuzzNoCustom(obj)
+			obj.Items = make([]KongService, c.Intn(10))
+			for i := range obj.Items {
+				c.Fuzz(&obj.Items[i])
+			}
+		},
+	}
+}
+
+// TestRoundTrip tests that the third-party kinds can be marshaled and unmarshaled correctly to/from JSON
+// without the loss of information. Moreover, deep copy is tested.
+func TestRoundTrip(t *testing.T) {
+	scheme := runtime.NewScheme()
+	codecs := serializer.NewCodecFactory(scheme)
+
+	AddToScheme(scheme)
+
+	seed := rand.Int63()
+	fuzzerFuncs := fuzzer.MergeFuzzerFuncs(metafuzzer.Funcs, apimFuzzerFuncs)
+	fuzzer := fuzzer.FuzzerFor(fuzzerFuncs, rand.NewSource(seed), codecs)
+
+	roundtrip.RoundTripSpecificKindWithoutProtobuf(t, SchemeGroupVersion.WithKind("KongService"), scheme, codecs, fuzzer, nil)
+	roundtrip.RoundTripSpecificKindWithoutProtobuf(t, SchemeGroupVersion.WithKind("KongRoute"), scheme, codecs, fuzzer, nil)
+}
