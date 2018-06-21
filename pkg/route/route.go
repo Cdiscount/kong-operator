734c6940f33ffd6f8fc6911c913a1be307239344
Etienne Coutaud <e.coutaud@gmail.com>
2018-06-21 10:32:27 +0200

    add route/test

--- pkg/route/route.go
+++ pkg/route/route.go
@@ -1,11 +1,14 @@
 package route
 
 import (
+	"io"
 	"net/http"
-	//"github.com/prometheus/client_golang/prometheus"
 )
 
 // Healthz simple healthcheck endpoint
 // HTTP/200
 func Healthz(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(http.StatusOK)
+	w.Header().Set("Content-Type", "application/json")
+	io.WriteString(w, `{"alive": true}`)
 }
